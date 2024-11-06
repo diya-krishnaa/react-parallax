ade9bd42294fe38d6d1e98de4389169cc968ab37
diya-krishnaa <diyakrishnaa01@gmail.com>
2024-11-06 09:41:06 +0530

    m1

---
+++ src/Textbox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Textbox = () => {
+  return (
+    <div id="textblock">
+    <div id="textblock-container">
+        <h1 id="textblock-title">What is Firewatch?</h1>
+        <p id="textblock-content">
+        The year is 1989.<br/><br/>
+        You are a man named Henry who has retreated from your messy life to work as a fire lookout in the Wyoming wilderness. Perched atop a mountain, it's your job to find smoke and keep the wilderness safe.<br/><br/>
+        An especially hot, dry summer has everyone on edge. Your supervisor, a woman named Delilah, is available to youat all times over a small, handheld radio—and is your only contact with the world you've left behind.<br/><br/>
+        But when an unsettling mystery lures you out of your lookout tower and into the rugged wilderness below, you'll navigate a wild and untamed landscape, confronting questions—and choices—that could strengthen or shatter the only meaningful connection you have left.
+        </p>
+    </div>
+</div>
+  )
+}
+
+export default Textbox
